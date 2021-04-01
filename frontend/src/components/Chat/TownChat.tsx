5d56e6890b35e4829520e03a64ee741886c66edc
srutibalan <balan.s@husky.neu.edu>
2021-04-01 18:17:53 -0400

    Add Use chat hook and socket.io

--- frontend/src/components/Chat/TownChat.tsx
+++ frontend/src/components/Chat/TownChat.tsx
@@ -1,11 +1,17 @@
 import {Widget} from "react-chat-widget";
 import React from "react";
 import {Button} from "@chakra-ui/react";
-
+import {useChat}  from '../../hooks/useChat';
 
 const TownChat = () => {
+
+  const { roomId } = props.match.params;
+  const { messages, sendMessage } = useChat(roomId);
+  const [newMessage, setNewMessage] = React.useState("");
+
   const handleNewUserMessage = (newMessage: string) => {
     console.log(`New message incoming! ${newMessage}`);
+    setNewMessage(event.target.value);
   };
 
   const getCustomLauncher = (handleToggle: any) =>
