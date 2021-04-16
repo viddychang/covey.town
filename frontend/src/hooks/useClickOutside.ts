c133f34f737b86212d311afcbf57bcf7ab1cf464
Ankita Mahapatra <ankitamaps03@gmail.com>
2021-04-16 13:27:18 -0500

    Fixed lint issues partially 2

--- frontend/src/hooks/useClickOutside.ts
+++ frontend/src/hooks/useClickOutside.ts
@@ -1,22 +1,23 @@
+// eslint:@typescript-eslint/no-explicit-any: "off"
 import { useEffect, useCallback, RefObject } from "react";
 
-const isRefArray = (r: RefObject<any> | RefObject<any>[]): r is RefObject<any>[] =>  "length" in r;
+const isRefArray = (r: RefObject<HTMLDivElement> | RefObject<HTMLDivElement>[]): r is RefObject<HTMLDivElement>[] =>  "length" in r;
 
 const isTarget = (ref1: RefObject<any>, event: MouseEvent) =>  ref1 && ref1.current && ref1.current.contains(event.target);
 
-const trueForAny = (array: React.RefObject<any>[], condition: (props: any) => boolean) =>
+const trueForAny = (array: React.RefObject<HTMLDivElement>[], condition: (props: RefObject<HTMLDivElement>) => boolean) =>
 
 array.reduce((conditionAlreadyMet, value) =>  conditionAlreadyMet || condition(value)
   , false);
 
 const useClickOutside = (
-  ref: RefObject<any> | RefObject<any>[],
+  ref: RefObject<HTMLDivElement> | RefObject<HTMLDivElement>[],
   onclick: () => void
-) => {
+) : RefObject<HTMLDivElement> | RefObject<HTMLDivElement>[]=> {
   const handleClick = useCallback(
     (click: MouseEvent) => {
       if (isRefArray(ref)) {
-        if (trueForAny(ref, (ref2: RefObject<any>) => isTarget(ref2, click))) {
+        if (trueForAny(ref, (ref2: RefObject<HTMLDivElement>) => isTarget(ref2, click))) {
           return;
         }
       } else if (isTarget(ref, click)) {
